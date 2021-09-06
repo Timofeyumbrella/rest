7d234ad2a4d7e63f6d7575237ccf80b5e912a6ac
Tsimafei Bykau <Tsimafei.Bykau@itechart-group.com>
2021-09-06 17:17:39 +0300

    remove email already in use from auth controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -8,8 +8,6 @@ const generateTokens = require("../utils/auth");
 
 const authController = {
   register: async ({ password, email, ...userFields }) => {
-    if (await User.findOne({ where: { email } })) throw new ApiError(403);
-
     return User.create({
       password: await bcrypt.hash(password, 10),
       email,
