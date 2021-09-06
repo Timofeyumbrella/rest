9dae080df4c8b6a46a4a68c04ca3926975ab192a
Tsimafei Bykau <Tsimafei.Bykau@itechart-group.com>
2021-09-06 17:05:28 +0300

    add unauthorized and unprocessable entity errors

--- backend/error/httpStatusCodes.js
+++ backend/error/httpStatusCodes.js
@@ -1,7 +1,9 @@
 const httpStatusCodes = {
   400: "Bad request",
+  401: "Unauthorized",
   404: "Not found",
   403: "Forbidden",
+  422: "Unprocessable Entity",
 };
 
 module.exports = httpStatusCodes;
