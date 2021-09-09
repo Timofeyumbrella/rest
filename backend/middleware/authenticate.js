585dfe13b3b2fb4d81c8cfe20ddd8111b258b604
Tsimafei Bykau <Tsimafei.Bykau@itechart-group.com>
2021-09-09 14:11:17 +0300

    add authenticate error message

--- backend/middleware/authenticate.js
+++ backend/middleware/authenticate.js
@@ -3,7 +3,7 @@ const ApiError = require("../error/ApiError");
 
 module.exports = (req, res, next) =>
   passport.authenticate("jwt", { session: false }, (_, user) => {
-    if (!user) throw new ApiError(403);
+    if (!user) throw new ApiError(403, "You have to login first");
 
     next();
   })(req, res, next);
