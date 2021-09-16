6abd4b47368b9b4814a61bbd89458a5d05c9331e
Tsimafei Bykau <Tsimafei.Bykau@itechart-group.com>
2021-09-16 13:45:34 +0300

    create user and event policies

--- backend/policies/eventPolicy.js
+++ backend/policies/eventPolicy.js
@@ -1,13 +1,55 @@
 const { Permission } = require("../models");
+const atob = require("atob");
 
 const eventPolicy = {
-  findAll: async () => true,
-  create: async () => {
+  findAll: async ({ authorization }) => {
+    const jwtUser = JSON.parse(atob(authorization.split(".")[1])).user;
+
     const permission = await Permission.findOne({
-      where: { roleId: 1, entity: "event" },
+      where: { roleId: jwtUser.roleId, entity: "event" },
+    });
+
+    return permission.dataValues.findAll === "any";
+  },
+
+  find: async ({ authorization }) => {
+    const jwtUser = JSON.parse(atob(authorization.split(".")[1])).user;
+
+    const permission = await Permission.findOne({
+      where: { roleId: jwtUser.roleId, entity: "event" },
+    });
+
+    return permission.dataValues.find === "any";
+  },
+
+  create: async ({ authorization }) => {
+    const jwtUser = JSON.parse(atob(authorization.split(".")[1])).user;
+
+    const permission = await Permission.findOne({
+      where: { roleId: jwtUser.roleId, entity: "event" },
     });
 
     return permission.dataValues.create === "any";
+  },
+
+  update: async ({ authorization }) => {
+    const jwtUser = JSON.parse(atob(authorization.split(".")[1])).user;
+
+    const permission = await Permission.findOne({
+      where: { roleId: jwtUser.roleId, entity: "event" },
+    });
+
+    return permission.dataValues.update === "any";
+  },
+
+  destroy: async ({ authorization }) => {
+    const jwtUser = JSON.parse(atob(authorization.split(".")[1])).user;
+
+    const permission = await Permission.findOne({
+      where: { roleId: jwtUser.roleId, entity: "event" },
+    });
+
+    return permission.dataValues.destroy === "any";
   },
 };
 
