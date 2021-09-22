1fb0f3a4c797964c5ff8564d22359e656a48bc98
Tsimafei Bykau <Tsimafei.Bykau@itechart-group.com>
2021-09-22 17:58:06 +0300

    add interceptor for mocking req res next

---
+++ backend/utils/interceptor.js
@@ -0,0 +1,21 @@
+module.exports = {
+  mockRequest: () => {
+    const req = {};
+
+    req.body = jest.fn().mockReturnValue(req);
+    req.params = jest.fn().mockReturnValue(req);
+    req.query = jest.fn().mockReturnValue(req);
+
+    return req;
+  },
+
+  mockResponse: () => {
+    const res = {};
+
+    res.status = jest.fn().mockReturnValue(res);
+    res.json = jest.fn().mockReturnValue(res);
+
+    return res;
+  },
+  mockNext: () => jest.fn(),
+};
