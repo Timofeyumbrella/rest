c2e14d99f3ae3447b85012df0660719b31dfa9d1
Tsimafei Bykau <Tsimafei.Bykau@itechart-group.com>
2021-10-05 09:49:26 +0300

    add homepage basic implementation

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -4,7 +4,11 @@ function Home() {
   return (
     <div className={styles.home}>
       <div className={styles.home__container}>
-        <h1 className={styles.home__title}>All events in one place</h1>
+        <h1 className={styles.home__title}>Welcome to ebox</h1>
+        <h2 className={styles.home__description}>
+          Here you can find all upcoming events here
+        </h2>
+        <div className={styles.home__spinner}></div>
       </div>
     </div>
   );
