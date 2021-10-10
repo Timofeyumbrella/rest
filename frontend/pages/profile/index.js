213ad6b313b5bc939e2682fcddef6e6442867dfc
Timofeyumbrella <timfrommit@gmail.com>
2021-10-10 03:03:53 +0300

    implement profile update

--- frontend/pages/profile/index.js
+++ frontend/pages/profile/index.js
@@ -1,9 +1,7 @@
 import { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
 
 import useAuth from "hooks/useAuth";
-import { setToken } from "redux/token/token.actions";
 
 import styles from "./Profile.module.scss";
 
@@ -16,9 +14,8 @@ function Profile() {
   const [gender, setGender] = useState("");
   const [password, setPassword] = useState("");
 
+  const { token } = useSelector((state) => state.token);
   const auth = useAuth();
-  const router = useRouter();
-  const dispatch = useDispatch();
 
   const handleNameChange = (event) => setName(event.target.value);
   const handleEmailChange = (event) => setEmail(event.target.value);
@@ -41,8 +38,7 @@ function Profile() {
       roleId,
     });
 
-    dispatch(setToken(""));
-    router.push("/user/login");
+    setEditMode(false);
   };
 
   useEffect(() => {
@@ -51,7 +47,7 @@ function Profile() {
     };
 
     getProfile();
-  }, []);
+  }, [token]);
 
   return (
     <div className={styles.profile}>
