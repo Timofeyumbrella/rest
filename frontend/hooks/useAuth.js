8d5787dab6433f9a6bd8dbeee81d48dc3b70ca6a
Tsimafei Bykau <Tsimafei.Bykau@itechart-group.com>
2021-10-12 13:14:45 +0300

    move login and register methods into useAuth hook

--- frontend/hooks/useAuth.js
+++ frontend/hooks/useAuth.js
@@ -1,42 +1,29 @@
-import { useSelector, useDispatch } from "react-redux";
-
-import atob from "atob";
-
-import { setToken } from "redux/token/token.actions";
 import getAxiosInstance from "utils/instance";
 
 function useAuth() {
-  const dispatch = useDispatch();
   const instance = getAxiosInstance();
 
-  const { token } = useSelector((state) => state.token);
-
-  const getProfile = () => JSON.parse(atob(token.split(".")[1])).user;
-
-  const updateUser = async ({
-    id,
-    name,
-    email,
-    age,
-    gender,
-    password,
-    roleId,
-  }) => {
-    const { data: res } = await instance.put(`/users/${id}`, {
-      name,
+  const login = async ({ email, password }) => {
+    const { data: res } = await instance.post("/user/login", {
       email,
-      age,
-      gender,
       password,
-      roleId,
     });
 
-    dispatch(setToken(res.data.access));
+    return res.data.access;
   };
 
+  const register = ({ name, age, email, gender, password }) =>
+    instance.post("/user/register", {
+      name,
+      age,
+      email,
+      gender,
+      password,
+    });
+
   return {
-    getProfile,
-    updateUser,
+    login,
+    register,
   };
 }
 
